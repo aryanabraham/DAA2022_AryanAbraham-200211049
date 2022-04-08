25a35df1e6cdd65832bfc2c3b43bcf797e197494
Aryan Abraham <abraham.aryan01@gmail.com>
2022-04-09 00:39:10 +0530

    Update 3.cpp

--- Week 1/3.cpp
+++ Week 1/3.cpp
@@ -1,44 +1,50 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
-int main()
-{
-    int i,j,n,m,key,flag=-1;
+void jSearch (int a[], int n, int key){
+    int i=0;
+    int x;
+    int comp=0;
+    int m=sqrt(n);
 
-    cout<<"Enter number of elements: ";
-    cin>>n;
-    int arr[n];
+    while(a[m]<=key&&m<n){
+        i=m;
+        m+=sqrt(n);
+        comp++;
 
-    for(i=0;i<n;i++)
-        cin>>arr[i];
-
-    m=sqrt(n);
-    cout<<"Enter key to be found: ";
-    cin>>key;   
-
-    for(i=0; i<n; i+=m)
-    {
-        if(arr[i]==key)
-        {
-            flag=i;
-            break;
-        }
-        else if(arr[i]>key)
-        {
-            for(j=(i/m)+1; j<i;j++)
-            {
-                if(arr[j]==key)
-                    flag=j;
-                    break;
-            }
-        }
+        if(m>n-1)
+            m=n;
     }
 
-    if(flag==-1)
-        cout<<"Key not found";
-    else
-        cout<<"Key found at %d index"<<flag;
+    for(x=i;x<m;x++){
+        comp++;
+        if(a[x]==key){
+            cout<<"Present "<<comp<<endl;
+            return;
+        }
 
-    return 0;
+    }
+
+    cout<<"Not Present"<<endl;
+}
+
+int main ()
+{
+    int i, n, t;
+    int key;
+
+    cin>>t;
+
+    while(t--){
+        cin>>n;
+        int a[n];
+
+        for(i=0;i<n;i++)
+            cin>>a[i];
+
+        cin>>key;
+
+        jSearch(a, n, key);
+    }
 }
