361cc31fd67ebe5aac27645b04f26bec075272b1
Aryan Abraham <abraham.aryan01@gmail.com>
2022-04-09 12:49:03 +0530

    Update 2.cpp

--- Week 3/2.cpp
+++ Week 3/2.cpp
@@ -1,16 +1,11 @@
-//Given an unsorted array of integers, design an algorithm and implement a program to sort this 
-//array using selection sort. Your program should also find number of comparisons and number of 
-//swaps required.
-
 #include <iostream>
 using namespace std;
 
-int sSort (int a[], int n){
-    int i;
-    int j;
-    int sml;
-    int pos;
+void sSort (int a[], int n){
+    int i, j;
+    int sml, pos;
     int comp=0;
+    int swap=0;
 
     for(i=0;i<n;i++){
         pos=i;
@@ -25,25 +20,28 @@ int sSort (int a[], int n){
         if(pos!=i){
             a[pos]=a[i];
             a[i]=sml;
+            swap++;
         }
     }
-    return comp;
+    for(i=0;i<n;i++)
+            cout<<a[i]<<" ";
+    cout<<"\ncomparisions = "<<comp;
+    cout<<"\nswaps = "<<swap<<endl;
 }
 
 int main ()
 {
-    int i;
-    int n;
+    int i, n, t;
 
-    cin>>n;
-    int a[n];
-    for(i=0;i<n;i++)
-        cin>>a[i];
+    cin>>t;
 
-    cout<<"Number of comparisions: "<<sSort(a, n);
-    cout<<"\nSorted array: ";
-    for(i=0;i<n;i++)
-        cout<<a[i]<<" ";
+    while(t--){
+        cin>>n;
+        int a[n];
+        for(i=0;i<n;i++)
+            cin>>a[i]; 
 
+        sSort(a, n);
+    }
     return 0;
 }
