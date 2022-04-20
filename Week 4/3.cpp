48d2b6f27c456bf99f338f05f24f81ef61d79d19
Aryan Abraham <abraham.aryan01@gmail.com>
2022-04-20 16:51:30 +0530

    Update 3.cpp

--- Week 4/3.cpp
+++ Week 4/3.cpp
@@ -1,31 +1,59 @@
 #include <iostream>
 using namespace std;
 
+void merge(int arr[],int l,int mid,int h)
+{
+    int count=0;
+    int i=l,j=mid+1;
+    int temp[h-l+1];
+    int k=0;
+    
+    while (i<=mid && j<=h){
+        if (arr[i]<arr[j])
+            temp[k++]=arr[i++];
+        else
+        {
+            temp[k++]=arr[j++];
+            count+=mid-i+1;
+        }
+    }
+    for (;i<=mid;)
+         temp[k++]=arr[i++];
+         
+    for (;j<=h;)
+         temp[k++]=arr[j++];
+         
+    for (int f=0;f<k;f++)
+        arr[f+l]=temp[f];
+}
+
+void merge_sort(int arr[],int l,int h)
+{
+    int mid;
+    if (l<h){
+        mid=l+(h-l)/2;
+        merge_sort(arr,l,mid);
+        merge_sort(arr,mid+1,h);
+        merge(arr,l,mid,h);
+    }
+}
+
 int main()
 {
-    int lrg, sml;
-    int a[5] = {2,4,3,9,3};
-    
-    lrg = a[0];
-    sml = a[1];
-    
-    if(sml > lrg)
+    bool flag;
+    int i, n, t, k;
+    cin>>t;
+    while (t--)
     {
-        lrg = a[1];
-        sml = a[0] ;
-    }
-    
-    for(int i = 2; i< 5; i++)
-    {
-        if(a[i] > lrg)
-          lrg = a[i];
+        cin>>n;
+        int a[n];
+        for (i=0;i<n;i++)
+           cin>>a[i];
         
-        if(a[i] < sml)
-          sml = a[i];
-       
-    }
+        cin>>k;
 
-    cout<<"Largest: "<<large;
-    cout<<"\nSmallest: "<<sml;
-    return 0;
+        merge_sort(a,0,n-1);
+        flag=0;
+        cout<<a[k]<<endl; 
+    }
 }
